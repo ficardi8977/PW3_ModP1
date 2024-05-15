95c89b2958a745685ecde43cbed1dd20a5cb0d96
Fernando Icardi <fernando.icardi@gmail.com>
2024-05-15 01:39:11 -0300

    ajuste para permitir inciar la aplicacion con una lista vacia

--- RegistroVentas/RegistroVentas/RegistroVentas_Logica/VentasService.cs
+++ RegistroVentas/RegistroVentas/RegistroVentas_Logica/VentasService.cs
@@ -4,11 +4,13 @@ namespace RegistroVentas_Logica
 {
     public class VentasService : IVentaService
     {
-        private IList<Venta> ventaList = new List<Venta>() { 
+        private IList<Venta> ventaList = new List<Venta>();
+
+        /*private IList<Venta> ventaList = new List<Venta>() { 
             new Venta() {IdVenta=1, Cliente = "Metagoles S.A", CantidadVendida= 250, PrecioUnitario = 20, TotalVenta = 5000 },
             new Venta() {IdVenta=2, Cliente = "MediaLunas SRL", CantidadVendida= 100, PrecioUnitario = 10, TotalVenta = 1000 },
             new Venta() {IdVenta=3, Cliente = "Teclieando S.A", CantidadVendida= 20, PrecioUnitario = 200, TotalVenta = 4000 },
-        };  
+        };  */
         public IList<Venta> GetVentas()
         {
             return ventaList;
@@ -17,7 +19,7 @@ namespace RegistroVentas_Logica
         public void AddVenta(Venta venta)
         {
             venta.TotalVenta = venta.CantidadVendida * venta.PrecioUnitario;
-            venta.IdVenta = ventaList.Max(x => x.IdVenta) + 1;
+            venta.IdVenta = ventaList.Count > 0 ? ventaList.Max(x => x.IdVenta) + 1 : 1;
             ventaList.Add(venta);
         }
 
